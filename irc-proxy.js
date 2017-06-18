981df778522189e1ec3ef2695b1ac011315c1f37
Weliton Marcos Pereira Lima <wlima@asert.com.br>
2017-06-18 17:49:11 -0300

    listener motd

--- irc-proxy.js
+++ irc-proxy.js
@@ -8,8 +8,8 @@ var amqp_ch;
 var irc_client;
 
 // Conexão com o servidor AMQP
-amqp.connect('amqp://localhost', function(err, conn) {
-	conn.createChannel(function(err, ch) {
+amqp.connect('amqp://localhost', function (err, conn) {
+	conn.createChannel(function (err, ch) {
 		amqp_conn = conn;
 		amqp_ch = ch;
 
@@ -17,35 +17,44 @@ amqp.connect('amqp://localhost', function(err, conn) {
 	});
 });
 
-function inicializar () {
+function inicializar() {
 
 	receberDoCliente("registro_conexao", function (msg) {
 
-		var id       = msg.id;
+		var id = msg.id;
 		var servidor = msg.servidor;
-		var nick     = msg.nick;
-		var canal    = msg.canal;
+		var nick = msg.nick;
+		var canal = msg.canal;
 
 		irc_client = new irc.Client(
-			servidor, 
+			servidor,
 			nick,
-			{channels: [canal]}
+			{ channels: [canal] }
 		);
 
-		irc_client.addListener('message'+canal, function (from, message) {
-		    
-		    console.log(from + ' => '+ canal +': ' + message);
-		    
-		    enviarParaCliente(id, {
-		    	"timestamp": Date.now(), 
+		irc_client.addListener('message' + canal, function (from, message) {
+
+			console.log(from + ' => ' + canal + ': ' + message);
+
+			enviarParaCliente(id, {
+				"timestamp": Date.now(),
 				"nick": from,
 				"msg": message
 			});
 		});
 
-		irc_client.addListener('error', function(message) {
-		    console.log('error: ', message);
+		irc_client.addListener('error', function (message) {
+			console.log('error: ', message);
 		});
+
+		irc_client.addListener('motd', function (message) {
+			enviarParaCliente(id, {
+				"timestamp": Date.now(),
+				"nick": nick,
+				"msg": message
+			});
+		});
+
 
 		proxies[id] = irc_client;
 	});
@@ -56,26 +65,26 @@ function inicializar () {
 	});
 }
 
-function receberDoCliente (canal, callback) {
+function receberDoCliente(canal, callback) {
 
-	amqp_ch.assertQueue(canal, {durable: false});
+	amqp_ch.assertQueue(canal, { durable: false });
 
-    console.log(" [irc] Waiting for messages in ", canal);
-    
-    amqp_ch.consume(canal, function(msg) {
+	console.log(" [irc] Waiting for messages in ", canal);
 
-      console.log(" [irc] Received %s", msg.content.toString());
-      callback(JSON.parse(msg.content.toString()));
+	amqp_ch.consume(canal, function (msg) {
 
-    }, {noAck: true});
+		console.log(" [irc] Received %s", msg.content.toString());
+		callback(JSON.parse(msg.content.toString()));
+
+	}, { noAck: true });
 }
 
-function enviarParaCliente (id, msg) {
+function enviarParaCliente(id, msg) {
 
 	msg = new Buffer(JSON.stringify(msg));
 
-	amqp_ch.assertQueue("user_"+id, {durable: false});
-	amqp_ch.sendToQueue("user_"+id, msg);
+	amqp_ch.assertQueue("user_" + id, { durable: false });
+	amqp_ch.sendToQueue("user_" + id, msg);
 	console.log(" [irc] Sent %s", msg);
 }
 
